bf0b0259360e3998913177ff2ec096a4aabd3903
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2025-03-08 17:29:45 +0200

    Base Entities

---
+++ LinkDev.IKEA.DAL/Common/Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Common.Entities
+{
+    internal class BaseEntity<Tkey>
+        where Tkey : IEquatable<Tkey>
+    {
+        public Tkey Id { get; set; }
+    }
+
+}
