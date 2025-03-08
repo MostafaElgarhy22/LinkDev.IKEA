5675d5fc8d1056d032f4471496ec99810e28bd5c
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2025-03-08 17:56:20 +0200

    Application DbContext

---
+++ LinkDev.IKEA.DAL/Persistence/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿
+using LinkDev.IKEA.DAL.Entities.Departments;
+using Microsoft.EntityFrameworkCore;
+
+namespace LinkDev.IKEA.DAL.Persistence.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=IKEA;Trusted_Connection=True;");
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
