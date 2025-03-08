670dade7d96f369cd3e1544f0ed4bc61c88fbcb1
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2025-03-08 18:57:33 +0200

    Department Module(Configurations)

---
+++ LinkDev.IKEA.DAL/Persistence/Data/Configurations/Departments/DepartmentConfigurations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinkDev.IKEA.DAL.Entities.Departments;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LinkDev.IKEA.DAL.Persistence.Data.Configurations.Departments
+{
+    internal class DepartmentConfigurations : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D => D.Id).UseIdentityColumn(10, 10);
+            builder.Property(D => D.Code).HasColumnType("varchar(10)");
+            builder.Property(D => D.Name).HasColumnType("varchar(100)");
+            builder.Property(D => D.Description).HasColumnType("varchar(100)");
+
+
+            builder.Property(E => E.CreatedBy).HasColumnType("varchar(50)");
+            builder.Property(E => E.LastModifiedBy).HasColumnType("varchar(50)");
+            builder.Property(E => E.CreationDate).HasDefaultValueSql("GETUTCDATE()");
+            builder.Property(E => E.LastModifiedOn).HasComputedColumnSql("GETUTCDATE()");
+        }
+    }
+}
