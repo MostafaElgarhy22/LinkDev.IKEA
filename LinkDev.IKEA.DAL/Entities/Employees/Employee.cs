62e5f76d9aa8962088ec71733986c075e21bf0b0
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2025-05-02 15:10:32 +0300

    Employee Entity and Configurations

---
+++ LinkDev.IKEA.DAL/Entities/Employees/Employee.cs
@@ -0,0 +1,21 @@
+﻿using LinkDev.IKEA.DAL.Common.Entities;
+using LinkDev.IKEA.DAL.Common.Enumss;
+
+namespace LinkDev.IKEA.DAL.Entities.Employees
+{
+    public class Employee : BaseAuditableEntity<int>
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public int? Age { get; set; }
+        public decimal Salary { get; set; }
+        public string? Address { get; set; }
+        public bool IsActive { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public DateOnly? HireDate { get; set; }
+        public Gender Gender { get; set; }
+        public EmployeeType EmployeeType { get; set; }
+        public string? Image { get; set; }
+    }
+}
---
+++ LinkDev.IKEA.DAL/Persistence/Data/Configurations/Employees/EmployeeConfigurations.cs
@@ -0,0 +1,39 @@
+﻿using LinkDev.IKEA.DAL.Common.Enumss;
+using LinkDev.IKEA.DAL.Entities.Employees;
+using LinkDev.IKEA.DAL.Persistence.Data.Configurations.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LinkDev.IKEA.DAL.Persistence.Data.Configurations.Employees
+{
+    internal class EmployeeConfigurations : BaseAuditableEntityConfiguration<int, Employee>
+    {
+        public override void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(E => E.Id).UseIdentityColumn(1, 1);
+            builder.Property(E => E.FirstName).HasColumnType("varchar(50)").IsRequired();
+            builder.Property(E => E.LastName).HasColumnType("varchar(50)").IsRequired();
+            builder.Property(E => E.Email).HasColumnType("varchar(100)");
+
+            builder.Property(E => E.Gender)
+                .HasConversion(
+
+                (gender) => gender.ToString(),
+                (gender) => Enum.Parse<Gender>(gender)
+                );
+
+            builder.Property(E => E.EmployeeType)
+                .HasConversion(
+                (employeeType) => employeeType.ToString(),
+                (employeeType) => Enum.Parse<EmployeeType>(employeeType)
+                );
+
+
+
+
+
+        }
+    }
+}
